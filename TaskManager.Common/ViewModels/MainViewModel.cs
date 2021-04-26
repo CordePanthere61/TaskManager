fda333a0e35624c9576eff0bcc49f4986d27a5d1
Julien Deguire <info@devactif.ca>
2021-04-25 21:50:01 -0400

    Add project files.

---
+++ TaskManager.Common/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SimpleMvvmToolkit.Express;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace TaskManager.Common
+{
+    public class MainViewModel : ViewModelBase<MainViewModel>
+    {
+
+        //public ObservableCollection<Result> AllProcesses { get; set;}
+
+        public MainViewModel()
+        {
+            //_processLoader = new ProcessLoader();
+            //LoadCurrentProcesses();
+        }
+
+        
+    }
+}
